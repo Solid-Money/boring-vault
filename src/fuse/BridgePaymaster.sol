79681181641104c1d9fe83418426f352cb0d4bf5
Mayank Mittal <msquare.pvt21@gmail.com>
2025-07-28 20:28:47 +0530

    [FIX]: audit changes

--- src/fuse/BridgePaymaster.sol
+++ src/fuse/BridgePaymaster.sol
@@ -4,50 +4,102 @@ pragma solidity ^0.8.21;
 import {UUPSUpgradeable} from "@oz/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@oz/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@oz/access/OwnableUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@oz/utils/ReentrancyGuardUpgradeable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract BridgePaymaster is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BridgePaymaster is
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
     /**
      * @notice Whether or not to sponsor transactions to contracts.
      */
-    mapping(address => bool) public isSponsored;
+    mapping(address => mapping(bytes4 => bool)) public isSponsored;
+
+    event SponsorUpdated(
+        address indexed target,
+        bytes4 indexed functionSig,
+        bool isSponsored
+    );
 
     error NotSponsored();
     error CallFailed();
+    error NativeTransferFailed();
+    error ERC20TransferFailed();
+    error InsufficientBalance();
+    error ERC20ApproveFailed();
 
-    modifier onlySponsored(address _target) {
-        if (!isSponsored[_target]) revert NotSponsored();
+    modifier onlySponsored(address _target, bytes4 _functionSig) {
+        if (!isSponsored[_target][_functionSig]) revert NotSponsored();
         _;
+    }
+
+    constructor() {
+        _disableInitializers();
     }
 
     function initialize(address initialOwner) public initializer {
         __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
     }
 
     function setSponsored(
         address _target,
+        bytes4 _functionSig,
         bool _isSponsored
     ) external onlyOwner {
-        isSponsored[_target] = _isSponsored;
+        isSponsored[_target][_functionSig] = _isSponsored;
+        emit SponsorUpdated(_target, _functionSig, _isSponsored);
     }
 
     function callWithValue(
         address target,
+        bytes4 functionSig,
         bytes calldata data,
         uint256 value
-    ) external onlySponsored(target) returns (bytes memory) {
+    )
+        external
+        nonReentrant
+        onlySponsored(target, functionSig)
+        returns (bytes memory)
+    {
+        if (value > address(this).balance) revert InsufficientBalance();
         (bool success, bytes memory ret) = target.call{value: value}(data);
         if (!success) revert CallFailed();
         return ret;
     }
 
     function rescueNative(address to) external onlyOwner {
-        payable(to).transfer(address(this).balance);
+        (bool success, ) = to.call{value: address(this).balance}("");
+        if (!success) revert NativeTransferFailed();
+    }
+
+    function rescueTokens(address token, address to) external onlyOwner {
+        (bool success, ) = token.call(
+            abi.encodeWithSelector(
+                IERC20.transfer.selector,
+                to,
+                IERC20(token).balanceOf(address(this))
+            )
+        );
+        if (!success) revert ERC20TransferFailed();
+    }
+
+    function approveERC20(
+        address token,
+        address spender,
+        uint256 amount
+    ) external onlyOwner {
+        (bool success, ) = token.call(
+            abi.encodeWithSelector(IERC20.approve.selector, spender, amount)
+        );
+        if (!success) revert ERC20ApproveFailed();
     }
 
     receive() external payable {}
-
-    fallback() external payable {}
 
     function _authorizeUpgrade(
         address newImplementation
